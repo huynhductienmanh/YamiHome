22edf1bf2c737a800a098bfc29935284d9be937b
Manh <huynhductienmanh@gmail.com>
2026-03-08 01:13:53 +0700

    update

--- yamihome.sql
+++ yamihome.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Máy chủ: localhost
--- Thời gian đã tạo: Th12 01, 2024 lúc 07:22 AM
--- Phiên bản máy phục vụ: 10.4.28-MariaDB
--- Phiên bản PHP: 8.2.4
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -17,11 +10,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Cơ sở dữ liệu: `vietchill`
---
-
--- --------------------------------------------------------
 
 --
 -- Cấu trúc bảng cho bảng `admin_cred`
